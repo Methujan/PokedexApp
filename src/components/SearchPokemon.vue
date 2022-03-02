e51eef870e008b23ef55c0bb790f1fecd3c11515
Methujan <methujan_02@hotmail.com>
2022-03-01 20:54:00 -0500

    add search bar

---
+++ src/components/SearchPokemon.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="search">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="term" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchPokemon",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      term: "",
+      result: {
+        name: String,
+        imgs: [],
+        height: Number,
+        weight: Number,
+        abilities: [],
+        types: [],
+      },
+    };
+  },
+  methods: {
+    getPokemon() {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.getTerm}`)
+        .then((response) => {
+          response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/pngwing.com.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SearchPokemon msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SearchPokemon from "@/components/SearchPokemon.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    SearchPokemon,
   },
 };
 </script>
